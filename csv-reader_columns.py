4494e31bbff51b7b42b605263fd4f0cc30926473
Migdalia Brito <migdalia.brito@calciumusa.com>
2016-03-04 10:16:09 -0500

    Logic sweep.

--- csv-reader_columns.py
+++ csv-reader_columns.py
@@ -7,16 +7,39 @@ test1 = open('testdata1.csv', 'r')
 test2 = open('testdata2.csv', 'r')
 
 with open('testdata1.csv', 'r') as test1, open('testdata2.csv', 'r') as test2:
-    reader1 = csv.reader(test1)
-    reader2 = csv.reader(test2)
+    reader1 = csv.reader(test1, delimiter=',', skipinitialspace=True)
+    reader2 = csv.reader(test2, delimiter=',', skipinitialspace=True)
 
+
+    #Lets count columns?
+
+    #CSV1
+    first_row_a = next(reader1)
+    num_cols_a = len(first_row_a)
+
+    #CSV2
+    first_row_b = next(reader2)
+    num_cols_b = len(first_row_b)
+
+
+
+    #If cols do not share same # of cols
+    while num_cols_a != num_cols_b:
+        lowestNum = min(num_cols_a, num_cols_b)
+
+        for i in range(lowestNum):
+            for x in reader1:
+                for y in reader2:
+                    if x[i] == y[i]:
+                        print x[i], y[i]
+
+    
     for x in reader1:
         #print rows[0], rows[1] #column1, column2
         for y in reader2:
             if x[0] == y[0]:
                 print "MATCH MATCH"
                 print x[0], y[0]
-
 
             if x[1] == y[1]:
                 print "MATCH MATCH 1"
