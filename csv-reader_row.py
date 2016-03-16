a4a631485d2425636dc300d35bf759a62f607230
Migdalia Brito <migdalia.brito@calciumusa.com>
2016-03-16 10:37:44 -0400

    Fixing row logic.

--- csv-reader_row.py
+++ csv-reader_row.py
@@ -13,20 +13,18 @@ with open('testdata1.csv', 'r') as test1, open('testdata2.csv', 'r') as test2:
 
     for samerows in reader1:
         for samerowstwo in reader2:
-            if samerows == samerowstwo:
-
+            while (samerows == samerowstwo):
                 counter += 1
-
                 if counter == 1:
                     print counter, " row match!"
                     print samerows, samerowstwo
-
-                elif counter == 0:
-                    print "No matching rows :("
-
                 else:
                     print counter, " rows match!"
                     print samerows, samerowstwo
+            else:
+                counter += 1
+                print 'Row', counter, "Does not match"
+
 
 
 #Close files
